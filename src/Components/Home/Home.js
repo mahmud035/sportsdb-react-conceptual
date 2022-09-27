57d6a8e8a00edcf4816f5767602eed980fd79d6f
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-27 16:13:35 +0600

    get search result dynamically using onChange evenet

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 
 const Home = () => {
+  const [search, setSearch] = useState('');
+  console.log(search);
+
   return (
     <div className="home-container">
       <div className="players-container">
         <h1>All Players</h1>
         <div className="search-area">
-          <input type="text" className="search-input" /> &nbsp;
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            className="search-input"
+          />
+          &nbsp;
           <button className="search-btn">Search</button>
         </div>
       </div>
